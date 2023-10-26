829f109b336f26b86ee5afbfde1e3856fcf51e4c
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-10-26 23:48:59 +0300

    result

---
+++ README.md
@@ -0,0 +1,4 @@
+1. https://ibb.co/jM845zz
+2. https://ibb.co/SwW2Hwq
+3. https://ibb.co/rQvb4S5
+4. https://ibb.co/GpwtQq5
---
+++ db/contacts.js
@@ -0,0 +1,62 @@
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const crypto = require("node:crypto");
+const { access } = require("node:fs");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const readContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf-8");
+
+  return JSON.parse(data);
+};
+
+const writeContacts = async (contacts) => {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+};
+
+const listContacts = async () => {
+  const contacts = await readContacts();
+
+  return contacts;
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await readContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return undefined;
+  }
+
+  const newContacts = [
+    ...contacts.slice(0, index),
+    ...contacts.slice(index + 1),
+  ];
+  await writeContacts(newContacts);
+
+  return contacts[index];
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await readContacts();
+  const newContact = { id: crypto.randomUUID(), name, email, phone };
+  contacts.push(newContact);
+  await writeContacts(contacts);
+
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
